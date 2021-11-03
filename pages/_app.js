97c71b0ab937923a75e74f0e61ddea42b4149d52
Junaed Abed <junaedabed@Junaeds-MacBook-Air.local>
2021-11-03 21:17:36 +0600

    photo added

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import theme from "../libs/theme";
 function Website({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
-      {/* <Fonts /> */}
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} alignContent="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.600", "whiteAlpha.300")}
+        p={3}
+        mb={6}
+        alignContent="center"
+      >
         Hello, I&apos;m a fullstack developer from Bangladesh!
       </Box>
 
@@ -13,6 +25,23 @@ const Page = () => {
             Md Junaed Abed
           </Heading>
           <p>(Software Developer / Designer / Mobile App developer)</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={1}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/junaed.jpg"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
