e262d8789af5d4993114c3f896082185f6de7680
JunaedAbed <junaedccpc17@gmail.com>
2021-11-27 01:48:11 +0600

    page transition added

---
+++ components/layouts/article.js
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import Head from "next/head";
+import { GridItemStyle } from "../grid-item";
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 30 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 30 },
+};
+
+const Layout = ({ children, title }) => (
+  <motion.article
+    initial="hidden"
+    animate="enter"
+    exit="exit"
+    variants={variants}
+    transition={{ duration: 0.5, type: "easeInOut" }}
+    style={{ position: "relative" }}
+  >
+    <>
+      {title && (
+        <Head>
+          <title>{title} - Junaed Abed</title>
+          {/* <meta name="twitter:title" content={title} />
+          <meta property="og:title" content={title} /> */}
+        </Head>
+      )}
+      {children}
+
+      <GridItemStyle />
+    </>
+  </motion.article>
+);
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import {
 } from "@chakra-ui/react";
 import Section from "../components/section";
 import Paragraph from "../components/paragraph";
+import Layout from "../components/layouts/article";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { BioSection, BioYear } from "../components/bio";
 import Skill from "../components/skill";
@@ -30,131 +31,133 @@ const Page = () => {
   ];
 
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue("whiteAlpha.600", "whiteAlpha.300")}
-        p={3}
-        mb={6}
-        alignContent="center"
-      >
-        Hello, I&apos;m a fullstack developer from Bangladesh!
-      </Box>
-
-      <Box display={{ md: "flex" }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Md Junaed Abed
-          </Heading>
-          <p>(Software Developer / Designer / Mobile App developer)</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
+          borderRadius="lg"
+          bg={useColorModeValue("whiteAlpha.600", "whiteAlpha.300")}
+          p={3}
+          mb={6}
+          alignContent="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={1}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/junaed.jpg"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a fullstack developer from Bangladesh!
         </Box>
-      </Box>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <p>
-          <Paragraph>
-            I&apos;m a software engineer based in Dhaka, Bangladesh with a
-            passion for web development. I&apos;m currently doing a Bachelor of
-            Science in Computer Science and Engineering from{" "}
+        <Box display={{ md: "flex" }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Md Junaed Abed
+            </Heading>
+            <p>(Software Developer / Designer / Mobile App developer)</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={1}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/junaed.jpg"
+              alt="Profile Image"
+            />
+          </Box>
+        </Box>
+
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <p>
+            <Paragraph>
+              I&apos;m a software engineer based in Dhaka, Bangladesh with a
+              passion for web development. I&apos;m currently doing a Bachelor
+              of Science in Computer Science and Engineering from{" "}
+              <Link href="https://www.bracu.ac.bd/" target="_blank">
+                Brac University
+              </Link>
+              . Currently I&apos;m working on my thesis based on Deep Learning
+              algorithm.
+            </Paragraph>
+            <Box align="center" my={4}>
+              <NextLink href="/works">
+                <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                  My Portfolio
+                </Button>
+              </NextLink>
+            </Box>
+          </p>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1998</BioYear>Born in Chittagong, Bangladesh.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>Completed Higher Secondary School Certificate
+            from{" "}
+            <Link href="http://ccpc.edu.bd/" target="_blank">
+              Chittagong Cantonment Public College
+            </Link>
+            .
+          </BioSection>
+          <BioSection>
+            <BioYear>2018 to now</BioYear>Completing Bachelor of Science in
+            Computer Science and Engineering from{" "}
             <Link href="https://www.bracu.ac.bd/" target="_blank">
               Brac University
             </Link>
-            . Currently I&apos;m working on my thesis based on Deep Learning
-            algorithm.
+            .
+          </BioSection>
+        </Section>
+
+        <Section delay={0.4}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            <Link
+              href="https://open.spotify.com/playlist/1VkO3YcqrjJ6ddSu6fljAo?si=eff93a93be2a4a0e"
+              target="_blank"
+            >
+              Music
+            </Link>
+            ,{" "}
+            <Link href="https://soundcloud.com/junaed-abed-98" target="_blank">
+              Playing Guitar
+            </Link>
+            ,{" "}
+            <Link href="https://github.com/JunaedAbed" target="_blank">
+              Coding
+            </Link>
+            , Football,{" "}
+            <Link href="https://www.instagram.com/mugdho.nef/" target="_blank">
+              Photography
+            </Link>
+            , Watching Series
           </Paragraph>
-          <Box align="center" my={4}>
-            <NextLink href="/works">
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                My Portfolio
-              </Button>
-            </NextLink>
-          </Box>
-        </p>
-      </Section>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1998</BioYear>Born in Chittagong, Bangladesh.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>Completed Higher Secondary School Certificate
-          from{" "}
-          <Link href="http://ccpc.edu.bd/" target="_blank">
-            Chittagong Cantonment Public College
-          </Link>
-          .
-        </BioSection>
-        <BioSection>
-          <BioYear>2018 to now</BioYear>Completing Bachelor of Science in
-          Computer Science and Engineering from{" "}
-          <Link href="https://www.bracu.ac.bd/" target="_blank">
-            Brac University
-          </Link>
-          .
-        </BioSection>
-      </Section>
-
-      <Section delay={0.4}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          <Link
-            href="https://open.spotify.com/playlist/1VkO3YcqrjJ6ddSu6fljAo?si=eff93a93be2a4a0e"
-            target="_blank"
-          >
-            Music
-          </Link>
-          ,{" "}
-          <Link href="https://soundcloud.com/junaed-abed-98" target="_blank">
-            Playing Guitar
-          </Link>
-          ,{" "}
-          <Link href="https://github.com/JunaedAbed" target="_blank">
-            Coding
-          </Link>
-          , Football,{" "}
-          <Link href="https://www.instagram.com/mugdho.nef/" target="_blank">
-            Photography
-          </Link>
-          , Watching Series
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.5}>
-        <Heading as="h3" variant="section-title">
-          Skills
-        </Heading>
-        <Skill>
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
-        </Skill>
-      </Section>
-    </Container>
+        <Section delay={0.5}>
+          <Heading as="h3" variant="section-title">
+            Skills
+          </Heading>
+          <Skill>
+            <ul className="skills-list">
+              {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+            </ul>
+          </Skill>
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 
