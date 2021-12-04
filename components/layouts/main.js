d2d1e35d8dc993562ccf4fe70ce6e68e3f97b56d
JunaedAbed <junaedccpc17@gmail.com>
2021-12-05 02:53:09 +0600

    404 page done

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,13 @@
 import { Box, Container } from "@chakra-ui/react";
 import Head from "next/head";
+import dynamic from "next/dynamic";
 import Navbar from "../navbar";
+import VoxelDogLoader from "../voxel-dog-loader";
+
+const LazyVoxelDog = dynamic(() => import("../voxel-dog"), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />,
+});
 
 const Main = ({ children, router }) => {
   return (
@@ -13,6 +20,7 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
         {children}
       </Container>
     </Box>
