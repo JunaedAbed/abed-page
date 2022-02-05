d6a313dffe0b49894e3b1522da64e1e599f4cc5a
JunaedAbed <junaedccpc17@gmail.com>
2022-02-05 14:57:49 +0600

    changed unused vars

--- pages/works/carpod.js
+++ pages/works/carpod.js
@@ -41,7 +41,7 @@ const Work = () => (
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </AspectRatio>
 
